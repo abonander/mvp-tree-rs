1946cb4e25e4d6cd5dfa550a735f961e12f9d214
Austin Bonander <austin@quiver.zone>
2018-09-27 20:26:00 -0700

    fix honggfuzz_iter

--- src/bin/honggfuzz_iter.rs
+++ src/bin/honggfuzz_iter.rs
@@ -8,7 +8,7 @@ use mvp_tree::MvpTree;
 fn main() {
     loop {
         fuzz!(|words: &[u16]| {
-            let words = words.collect::<Vec<u16>>();
+            let words = words.iter().cloned().collect::<Vec<u16>>();
             let mut tree = MvpTree::new(|&l: &u16, &r| (l ^ r).count_ones() as u64);
             tree.extend(words.iter().cloned());
             words.sort();
