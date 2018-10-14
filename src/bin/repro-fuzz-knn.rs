0882a8cbd56d80d592236da4f5a96786f7c8e7c2
Austin Bonander <austin.bonander@gmail.com>
2018-10-13 19:23:37 -0700

    add `repro-fuzz-knn`

---
+++ src/bin/repro-fuzz-knn.rs
@@ -0,0 +1,21 @@
+extern crate mvp_tree;
+
+use std::env;
+use std::fs::File;
+use std::io::{self, Read};
+
+// created to reproduce panics outsize of `libFuzzer`
+// https://github.com/rust-fuzz/libfuzzer-sys/issues/36
+fn main() {
+    let mut data = Vec::new();
+
+    if let Some(path) = env::args().nth(1) {
+        let mut file = File::open(&path).expect(&format!("failed to open file: {}", path));
+        file.read_to_end(&mut data).expect("failed to read file");
+    } else {
+        let mut stdin = io::stdin();
+        stdin.lock().read_to_end(&mut data).expect("failed to read data from stdin");
+    }
+
+    mvp_tree::fuzz_knn(&data);
+}
